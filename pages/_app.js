6337993c8ee76b6b51f0e8b37b470abe1ece3753
salla <Siva.Alla@phoenix.edu>
2022-01-25 09:34:06 -0700

    test the build

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () =>{
+    return(
+        <h2>App home page</h2>
+    )
+}
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+
+      <Head>
+        <title>My Career</title>
+        <meta name="description" content="Job recommendation app for my career" />
+        <link rel="icon" type="images/png" href="/icon-192x192.png" />
+      </Head>
+
+
+  )
+}
